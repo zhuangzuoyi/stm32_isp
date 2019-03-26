f81d4ce8da07ee543039f0a7f606e3c5d8e11db6
zhuangliuye <2335217760@qq.com>
2019-03-26 18:27:27 +0800

    test read write

--- stm32_isp/mainwindow.cpp
+++ stm32_isp/mainwindow.cpp
@@ -17,14 +17,125 @@ MainWindow::MainWindow(QWidget *parent) :
         QAction *action = new QAction(menu_temp,this);
         if(started ==  false)
         {
-            port = menuBar()->addMenu(menu_temp);
+            port = menuBar()->addMenu("PORT:" + info.portName());
+            current_port = info.portName();
             started = true;
         }
         port->addAction(action);
     }
+    connect(port,SIGNAL(triggered(QAction*)),this,SLOT(port_menu_click(QAction*)));
+    QStringList Baudrate;
+    Baudrate.append("460800");
+    Baudrate.append("256000");
+    Baudrate.append("230400");
+    Baudrate.append("128000");
+    Baudrate.append("115200");
+    Baudrate.append("76800");
+    Baudrate.append("57600");
+    Baudrate.append("38400");
+    Baudrate.append("19200");
+    Baudrate.append("14400");
+    Baudrate.append("9600");
+    baudrate_menu = menuBar()->addMenu("bsp:" + Baudrate.at(0));
+    for(QString baud:Baudrate)
+    {
+        QAction *action = new QAction(baud,this);
+        baudrate_menu->addAction(action);
+//        qDebug()<<baud;
+    }
+   connect(baudrate_menu,SIGNAL(triggered(QAction*)),this,SLOT(baud_menu_click(QAction*)));
+
+   current_baudrate = Baudrate.at(0).toInt();
+   serial_port_init();
+   connect(&serial, &QSerialPort::readyRead, this,&MainWindow::serial_read_dat);
 }
+
+
+void MainWindow::serial_read_dat(void)
+{
+    qDebug()<<serial.readAll();
+//    qDebug()<<
+}
+
+
+void MainWindow::port_menu_click(QAction* act)
+{
+//    qDebug()<<"Port action click: "<<act->text();
+    for(QAction *action:port->actions())
+    {
+        action->setIcon(QIcon::fromTheme("document-save", QIcon(":/images/none.png")));
+
+//        qDebug()<<action->text();
+    }
+    current_port = act->text().split(":").at(0);
+    act->setIcon(QIcon::fromTheme("document-save", QIcon(":/images/select.png")));
+    port->setTitle("PORT : " + act->text().split(":").at(0));
+}
+
+
+void MainWindow::baud_menu_click(QAction* act)
+{
+    qDebug()<<act->text();
+    current_baudrate = act->text().toInt();
+    for(QAction *action:baudrate_menu->actions())
+    {
+        action->setIcon(QIcon::fromTheme("document-save", QIcon(":/images/none.png")));
+
+    }
+    act->setIcon(QIcon::fromTheme("document-save", QIcon(":/images/select.png")));
+    baudrate_menu->setTitle("bps:" + act->text());
+    serial.setBaudRate(current_baudrate);
+}
+
+
+void MainWindow::serial_port_init(void)
+{
+
+    serial.setDataBits(QSerialPort::Data8);
+    serial.setParity(QSerialPort::NoParity);
+    serial.setStopBits(QSerialPort::OneStop);
+
+    serial.setPortName(current_port);
+    serial.setBaudRate(current_baudrate);
+    qDebug()<<port->title().split(":").at(1);
+    qDebug()<<baudrate_menu->title().split(":").at(1);
+}
+
 
 MainWindow::~MainWindow()
 {
+    serial.close();
     delete ui;
+}
+
+void MainWindow::serial_open(void)
+{
+    if(serial.isOpen() == false || serial.portName() != current_port || serial.baudRate() != current_baudrate)
+    {
+        serial.close();
+        serial.setPortName(current_port);
+        serial.setBaudRate(current_baudrate);
+        if (!serial.open(QIODevice::ReadWrite)) {
+            qDebug()<<"Open "<<current_port<<" faile";
+            return;
+        }
+    }
+}
+void MainWindow::on_pushButton_clicked()
+{
+    serial_open();
+    char start = 0x7f;
+    serial.write(&start,1);
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    serial.close();
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    serial_open();
+    uint8_t start[2] = {0x00,0xff};
+    serial.write((const char *)&start,2);
 }
--- stm32_isp/mainwindow.h
+++ stm32_isp/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "QSerialPort"
 
 namespace Ui {
 class MainWindow;
@@ -14,10 +15,28 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void serial_open(void);
+private slots:
+    void port_menu_click(QAction* act);
+    void baud_menu_click(QAction* act);
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void serial_read_dat(void);
+    void on_pushButton_3_clicked();
 
 private:
     Ui::MainWindow *ui;
     QMenu *port;
+    QMenu *baudrate_menu;
+
+    QString current_port;
+    int current_baudrate;
+
+    QSerialPort serial;
+
+    void serial_port_init(void);
 };
 
 #endif // MAINWINDOW_H
